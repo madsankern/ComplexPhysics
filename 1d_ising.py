355ee4dd4c6ae1ff9c638382f05e13d0b0a784cf
madsankern <jqk218@econ.ku.dk>
2021-09-16 18:08:39 +0200

    Initial commit

---
+++ 1d_ising.py
@@ -0,0 +1,10 @@
+###############################
+## Code used for the midterm ##
+###############################
+# Implements the one-dimensional Ising model with either 2 or 4 nearest neighbours
+
+## 2 neighbour case first##
+
+
+
+def ising(J,)
